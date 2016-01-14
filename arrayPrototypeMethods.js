3523064e132bf5c874643f160bb489432f5b0e9a
SaintEmbers <quantum108@gmail.com>
2016-01-14 12:11:05 -0800

    array prototype methods

---
+++ arrayPrototypeMethods.js
@@ -0,0 +1,14 @@
+Array.prototype.all = function (p) {
+  return this.reduce( (result, current) =>
+    !result ? result : p(current), true);
+};
+
+Array.prototype.none = function (p) {
+  return this.reduce( (start, current) =>
+    !start ? start : !p(current), true);
+};
+
+Array.prototype.any = function (p) {
+  return this.reduce(function(result, current)
+    result ? result : p(current), false);
+};
