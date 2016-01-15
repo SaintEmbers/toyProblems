e46c979497e8b0d943cbc6e98801edaee4fa47dc
SaintEmbers <quantum108@gmail.com>
2016-01-15 12:53:35 -0800

    reverse array && make available

---
+++ utilities.js
@@ -0,0 +1,21 @@
+//reverse a string in less than linear time
+
+var reverse = function(array) {
+  var start = 0;
+  var end = array.length - 1;
+  var mid = Math.floor(array.length/2);
+  while(start <= mid && end >= mid){
+    var temp = array[start];
+    array[start] = array[end];
+    array[end] = temp;
+    start++;
+    end--;
+  }
+return array;
+}
+
+
+//make a function that will call another function when you need it
+var make_available = function (fn) {
+  return fn.bind.apply(fn, arguments);
+};
