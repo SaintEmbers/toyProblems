fee14ab9cfaa21609ceee9ff9f322e839785c5d8
SaintEmbers <quantum108@gmail.com>
2016-01-25 08:33:45 -0800

    Add coin sum solution

--- recursion.js
+++ recursion.js
@@ -83,3 +83,45 @@ function connectFour(board) {
   }
   return openSpaces ? 'in progress' : 'draw'
 }
+
+
+// Write a function that counts how many different ways you can make change for an amount of money,
+// given an array of coin denominations.
+// For example, there are 3 ways to give change for 4 if you have coins with denomination 1 and 2...
+var countChange = function(money, coins) {
+  // your implementation here
+  var counter = 0;
+  coins.sort(function(a,b){return a-b});
+  (function count(index, remainder){
+    var coin = coins[index]
+    console.log(index, remainder)
+    if(index === 0 && remainder % coin === 0){
+      counter++
+      return
+    }
+    if(index === 0 && remainder % coin !==0){
+      return
+    }
+    while(remainder >= 0){
+      count(index-1, remainder)
+      remainder -= coin
+    }
+  })(coins.length -1, money)
+  return counter
+}
+
+
+var countChange = function(money, coins) {
+  if(money < 0 || coins.length === 0)
+    return 0
+  else if(money === 0)
+    return 1
+  else
+    return countChange(money - coins[0], coins) + countChange(money, coins.slice(1))
+}
+
+
+
+
+
+
