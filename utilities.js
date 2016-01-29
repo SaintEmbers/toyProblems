4b0c688a54819dd77ad03877fc2b8da7fc57447f
SaintEmbers <mailjasoncollins@gmail.com>
2016-01-28 22:14:28 -0800

    Add addDigits function

--- utilities.js
+++ utilities.js
@@ -74,6 +74,25 @@ function toBinary(n){
   return bitstring
 }
 
+//Given a non-negative integer num,
+//repeatedly add all its digits until the result has only one digit.
+
+//For example:
+//Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2.
+//Since 2 has only one digit, return it.
+var addDigits = function(num) {
+    num = num.toString()
+
+    while(num.length > 1){
+        var total = 0
+        for(var i = 0; i < num.length; i++){
+            total += +num[i]
+        }
+         return addDigits(total)
+    }
+    return +num
+};
+
 
 
 
